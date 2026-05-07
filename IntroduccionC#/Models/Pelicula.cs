33afce0f2aaf231b5abba4d6551a53b288059acd
Uriel444-web <urielhenandez123@gmail.com>
2026-05-06 20:58:30 -0600

    modelos 06/05/2026

---
+++ IntroduccionC#/Models/Pelicula.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Pelicula
+{
+	public string Titulo { get; set; }
+	public string Duracion { get; set; }
+	public DateTime Publicacion { get; set; }
+	public string Pais {  get; set; }
+}
